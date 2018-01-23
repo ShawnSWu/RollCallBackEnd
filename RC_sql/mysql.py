b796c2b1171cd7a8fe8103bb15893a33583ae873
Shawn Wu <swshawnwu@gmail.com>
2018-01-24 00:52:40 +0800

    rollcall backend

--- RC_sql/mysql.py
+++ RC_sql/mysql.py
@@ -1,13 +1,28 @@
 import MySQLdb
 
+host = "us-cdbr-iron-east-05.cleardb.net"
+user = "bf4c0cc20ecc57"
+passwd = "a4f21c13"
+db_name = "heroku_135deb52640a216"
+
 
 def mysql_command(execute_command):
-    db = MySQLdb.connect(host="us-cdbr-iron-east-05.cleardb.net",
-                         user="bf4c0cc20ecc57", passwd="a4f21c13", db="heroku_135deb52640a216")
+    db = __mysql_connect(execute_command)
     cursor = db.cursor()
-    sql_command = execute_command
-    cursor.execute(sql_command)
-
     results = cursor.fetchall()
     db.close()
     return results
+
+
+def mysql_insert_data(execute_command):
+    db = __mysql_connect(execute_command)
+    db.commit()
+    db.close()
+
+
+def __mysql_connect(execute_command):
+    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db_name)
+    cursor = db.cursor()
+    sql_command = execute_command
+    cursor.execute(sql_command)
+    return db
