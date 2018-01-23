bd718a447bf32e9e9b9d5985cc7ce97e25c71b0b
Shawn Wu <swshawnwu@gmail.com>
2018-01-23 17:59:40 +0800

    rollcall backend

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Application import create_app
 import os
 
 app = create_app()
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 33507))
 
 # 確保被導入時不執行
 if __name__ == '__main__':
