8a86a91ec9d5b719b6f1fed5fea9586abba6403a
Shawn Wu <swshawnwu@gmail.com>
2018-01-24 00:56:19 +0800

    rollcall backend

--- RC_sql/mysql.py
+++ RC_sql/mysql.py
@@ -7,22 +7,19 @@ db_name = "heroku_135deb52640a216"
 
 
 def mysql_command(execute_command):
-    db = __mysql_connect(execute_command)
+    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db_name)
     cursor = db.cursor()
+    sql_command = execute_command
+    cursor.execute(sql_command)
     results = cursor.fetchall()
     db.close()
     return results
 
 
 def mysql_insert_data(execute_command):
-    db = __mysql_connect(execute_command)
-    db.commit()
-    db.close()
-
-
-def __mysql_connect(execute_command):
     db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db_name)
     cursor = db.cursor()
     sql_command = execute_command
     cursor.execute(sql_command)
-    return db
+    db.commit()
+    db.close()
