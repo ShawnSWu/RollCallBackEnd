e0bf86b9038eca4625a775f13596a96f794b42b4
Shawn Wu <swshawnwu@gmail.com>
2018-02-27 01:08:17 +0800

    insert data

--- ListData/ListApi.py
+++ ListData/ListApi.py
@@ -90,7 +90,7 @@ def insert_newData_to_oldList():
     for data in data_list:
         # data type is dictionary
         for key in data.keys():
-            sql_command = " insert into user_list_info values ('%s', '%s', '%s', '%s', '%s') ," % (str(account), str(list_name), str(key), str(data[key]), str(image_uri))
+            sql_command = " insert into user_list_info values ('%s', '%s', '%s', '%s', '%s') " % (str(account), str(list_name), str(key), str(data[key]), str(image_uri))
             result = mysql_command(sql_command)
             insert_result = result
 
