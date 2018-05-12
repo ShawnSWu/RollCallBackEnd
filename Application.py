abc81a8074ac633dd80e0dc8ed7628832cd6d428
Shawn Wu <swshawnwu@gmail.com>
2018-05-12 13:37:46 +0800

    add todo feature

--- Application.py
+++ Application.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from Login.accountManger import account_Request
 from ListData.ListApi import list_Request
+from ToDo_NoteAPI.ToDoApi import todo_request
+
 
 def create_app():
     app = Flask(__name__)
@@ -11,4 +13,5 @@ def create_app():
 
     app.register_blueprint(account_Request, url_prefix="/account")
     app.register_blueprint(list_Request, url_prefix="/list")
+    app.register_blueprint(todo_request, url_prefix="/todo")
     return app
---
+++ ToDo_NoteAPI/ToDoApi.py
@@ -0,0 +1,64 @@
+from flask import Blueprint
+from flask import request, abort
+from RC_sql.mysql import *
+import json
+
+todo_request = Blueprint('todo_request', __name__)
+
+
+@todo_request.route("/gettododate", methods=['POST'])
+def get_todo_data():
+    if not request.json:
+        abort(404)
+
+    json_dict = request.json
+    account = json_dict['account']
+    password = json_dict['password']
+    sql_command = "select todo_title,todo_createtime,todo_isFinsh from todo_note where account = '%s' " % (
+        str(account))
+    result = get_mysql_data(sql_command)
+
+    return_list = []
+
+    if result is not None:
+        for data in result:
+            item_json = {'todo_title': data[0], 'todo_createtime': data[1], 'todo_isFinsh': data[2]}
+            return_list.append(item_json)
+
+    return json.dumps(return_list, ensure_ascii=False)
+
+
+@todo_request.route("/createnewtodo", methods=['POST'])
+def create_new_todo():
+    if not request.json:
+        abort(404)
+
+    json_dict = request.json
+    account = json_dict['account']
+    password = json_dict['password']
+    todo_title = json_dict['todo_title']
+    todo_createtime = json_dict['todo_createtime']
+    todo_isFinsh = json_dict['todo_isFinsh']
+
+    sql_command = "insert into todo_note values('%s','%s','%s',%d) " % (
+        str(account), str(todo_title), str(todo_createtime), todo_isFinsh)
+    result = mysql_command(sql_command)
+
+    return json.dumps(result, ensure_ascii=False)
+
+
+@todo_request.route("/finshtodo", methods=['POST'])
+def finsh_todo():
+    if not request.json:
+        abort(404)
+
+    json_dict = request.json
+    account = json_dict['account']
+    password = json_dict['password']
+    todo_title = json_dict['todo_title']
+
+    sql_command = "update todo_note set todo_isFinsh = 1 where account = '%s' and todo_title = '%s' " % (
+        str(account), str(todo_title))
+    result = mysql_command(sql_command)
+
+    return json.dumps(result, ensure_ascii=False)
